4cb5d3754c5340a52ed160ae5cdaa2afca927e0f
Joe Ferrer <joe@theforage.com>
2023-03-01 00:45:20 +1100

    Move python files to datafeed dir

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -29,18 +30,28 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+      if (this.state.showGraph) {
     return (<Graph data={this.state.data}/>)
+  }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
+      let x = 0;
+      const interval = setInterval(() => {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+      this.setState({
+          data: serverResponds,
+          showGraph: true,
     });
+  });
+  x++;
+  if (x > 1000){
+      clearInterval(interval);
+      }
+  },100);
   }
 
   /**
@@ -53,7 +64,7 @@ class App extends Component<{}, IState> {
           Bank & Merge Co Task 2
         </header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button"
+          <button className="btn btn-primary Stream-button">
             // when button is click, our react app tries to request
             // new data from the server.
             // As part of your task, update the getDataFromServer() function
